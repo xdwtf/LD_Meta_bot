84c7cf866f019ace50f17cb05f7c299ccc552326
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2021-11-21 16:31:15 +0530

    Update herokuctrl.py

--- helpers/herokuctrl.py
+++ helpers/herokuctrl.py
@@ -9,10 +9,7 @@ import math
 from config import Config
 
 BOT_TOKEN = Config.BOT_TOKEN
-LD_DOMAIN = Config.LD_DOMAIN
-SECRET = Config.SECRET
 ADMIN_IDS = Config.ADMIN_IDS
-PIC = Config.PIC
 HEROKU_API_KEY = Config.HEROKU_API_KEY
 HEROKU_APP_NAME = Config.HEROKU_APP_NAME
 HEROKU_API_KEYX = Config.HEROKU_API_KEYX
