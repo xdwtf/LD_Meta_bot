637092be90e310fbf6047a09fcb285fee6dac9c0
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2021-11-21 16:18:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,9 @@ import os
 # CLASSES
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    LD_DOMAIN = os.environ.get("LD_DOMAIN", "")
-    SECRET = os.environ.get("SECRET", "")
     ADMIN_IDS = os.environ.get("ADMIN_IDS", "")
-    PIC = os.environ.get("PIC", "")
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", "")
-    GROUP_IDS = os.environ.get("GROUP_IDS", "")
+    HEROKU_API_KEYX = os.environ.get("HEROKU_API_KEYX", "")
+    HEROKU_APP_NAMEX = os.environ.get("HEROKU_APP_NAMEX", "")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    GROUP_CMDS = os.environ.get("GROUP_CMDS", "")
