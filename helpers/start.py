a4cbd6335beecbe064fbe4bc5fb78fd251100528
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2021-11-21 15:48:12 +0530

    NO

--- helpers/start.py
+++ helpers/start.py
@@ -8,12 +8,7 @@ import time
 from config import Config
 
 BOT_TOKEN = Config.BOT_TOKEN
-LD_DOMAIN = Config.LD_DOMAIN
-SECRET = Config.SECRET
 ADMIN_IDS = Config.ADMIN_IDS
-PIC = Config.PIC
-HEROKU_API_KEY = Config.HEROKU_API_KEY
-HEROKU_APP_NAME = Config.HEROKU_APP_NAME
 
 try:
     ADMIN_LIST = ADMIN_IDS 
@@ -46,8 +41,5 @@ def get_readable_time(seconds: int) -> str:
 
 def startmessage(m, botStartTime):
     uptime = get_readable_time((time.time() - botStartTime))
-    start_string = "Hi ! Welcome to Libdrive Manager Bot by [Shreyansh Rawal](telegram.me/s_rawal) !\n\n*I'm Alive Since : *`" + uptime + "`\n\n ✯ *For More Info*, Send /help to the Bot !!\n\n ✯ *Also Read the Important Instructions* by clicking the Instructions Button in /help !!"
-    if len(PIC.replace(" ", "")) != 0:
-        bot.send_photo(m.chat.id, PIC, caption = start_string, parse_mode=telegram.ParseMode.MARKDOWN)
-    else:
-        bot.send_message(m.chat.id, start_string, parse_mode=telegram.ParseMode.MARKDOWN)
+    start_string = "Hi I'm Alive Since: " + uptime + "xd"
+    bot.send_message(m.chat.id, start_string, parse_mode=telegram.ParseMode.MARKDOWN)
