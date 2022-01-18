97cd3432a286e26dd14879ab31fd09bcf6c7449e
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2022-01-18 16:10:15 +0530

    Update signal.py

--- helpers/signal.py
+++ helpers/signal.py
@@ -24,7 +24,10 @@ ADMIN_IDS = Config.ADMIN_IDS
 
 try:
     ADMIN_LIST = ADMIN_IDS
-    restricted_mode = len(ADMIN_LIST) != 0
+    if len(ADMIN_LIST) != 0:
+        restricted_mode = True
+    else:
+        restricted_mode = False
 except:
     ADMIN_LIST = []  # ==> Do Not Touch This !!
     restricted_mode = False
@@ -43,6 +46,8 @@ class SIG():
         for i in CHAT_IDS:
             if len(i) != 0 and i.isnumeric() == True:
                 bot.send_message(int(i), "*SIGTERM* Received\n\nExiting With Status Code : *0*\n\n`The BOT will Shut Down Now !`", parse_mode=telegram.ParseMode.MARKDOWN)
+            else:
+                pass
         sys.exit(1)
 
     def sigint_handler(signum, frame):
@@ -50,4 +55,6 @@ class SIG():
         for i in CHAT_IDS:
             if len(i) != 0 and i.isnumeric() == True:
                 bot.send_message(int(i), "*SIGINT* Received\n\nExiting With Status Code : *0*\n\n`The BOT will Shut Down Now !`", parse_mode=telegram.ParseMode.MARKDOWN)
+            else:
+                pass
         sys.exit(143)
